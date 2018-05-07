326768f8c7ef532afafa789d248fdd25fe36e164
jimmyramirez91 <jimmyramirez91@gmail.com>
2018-05-07 09:35:46 -0500

    Cambios en el webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require("webpack");
+var path = require("path");
 
 //Nombre de la dirección final donde se publicara el componente
-var BUILD_DIR = path.resolve(__dirname, 'public');
+var BUILD_DIR = path.resolve(__dirname, "public");
 // var BUILD_DIR = path.resolve(__dirname, 'public/minutas');
-var APP_DIR = path.resolve(__dirname, 'app');
+var APP_DIR = path.resolve(__dirname, "app");
 
 var config = {
-    entry: APP_DIR + '/index.js',
-    output: {
-        path: BUILD_DIR,        
-        filename: 'table-tic.js'
-        //filename: 'bundle.js'
-    }, module: {
-        loaders: [
-            {
-                test: /\.js|\.jsx?/,
-                include: APP_DIR,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
-        ]
-    }
+  entry: APP_DIR + "/index.js",
+  output: {
+    path: BUILD_DIR,
+    filename: "table-tic.js"
+    //filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js|\.jsx?/,
+        include: APP_DIR,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
 };
 
 module.exports = config;
